4b903ca510be7069f8c493bd9ff9944a6fd1bc5b
施永辉 <2786250969@qq.com>
2018-11-27 13:59:43 +0800

    项目提交

---
+++ Indepandent/Indepandent/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Indepandent
+{
+    public class BundleConfig
+    {
+        // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            // 生产准备就绪，请使用 https://modernizr.com 上的生成工具仅选择所需的测试。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css"));
+            bundles.Add(new StyleBundle("~/bundles/show").Include(
+                      "~/Scripts/show.js"));
+            bundles.Add(new StyleBundle("~/bundles/StoreStart").Include(
+                       "~/Scripts/StoreStart.js"));
+            bundles.Add(new StyleBundle("~/Content/syhstyle").Include(
+                       "~/Content/syhstyle.css",
+                       "~/Content/FlexSlide.css"));
+        }
+    }
+}
---
+++ Indepandent/Indepandent/Controllers/IndexController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Indepandent.Models;
+using Indepandent.Models.IRepository;
+using Indepandent.Models.Repository;
+using qwjcode;
+
+namespace Indepandent.Controllers
+{
+    public class IndexController : Controller
+    {
+        ProjectEntities db = new ProjectEntities();
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Index2()
+        {
+            IGame_deRepository da = new Game_deRepository();
+            var dt = da.FindTop();
+            return PartialView(dt);
+        }
+       
+        
+        [AllowAnonymous]
+        public ActionResult GetValidateCode()
+        {
+            ValidateCode vCode = new ValidateCode();
+            string code = vCode.CreateValidateCode(4);
+            Session["ValidateCode"] = code;
+            byte[] bytes = vCode.CreateValidateGraphic(code);
+            return File(bytes, @"image/jpeg");
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(userinfo use)
+        {
+            if (use.valicode != Session["ValidateCode"].ToString())
+            {
+                return Content("<script>alert('验证码错误');window.location.href='Index';</script>");
+            }
+            if (ModelState.IsValid)
+            {
+                IUserRepository da = new UserRepository();
+                da.Add(use);
+                Session["name"] = use.username;
+                return RedirectToAction("Index","Store");
+            }
+           
+            return PartialView(use);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(userinfo user)
+        {
+
+            
+           if(user.valicode!=Session["ValidateCode"].ToString())
+            {
+                return Content("<script>alert('验证码错误');window.location.href='Index';</script>");
+            }
+           
+            
+                var use = db.userinfo.Where(o => o.username == user.username).Where(o => o.userpassword == user.userpassword);
+                if (use.Count() == 0)
+                {
+                    Session["name"] = null;
+                    return Content("<script>alert('用户密码错误');window.location.href='Index';</script>");
+
+                }
+                else
+                {
+
+                    Session["name"] = user.username;
+                    return Content("<script>alert('登录成功');window.location.href='Index';</script>");
+                }
+            
+            
+           
+        }
+        
+    }
+}
